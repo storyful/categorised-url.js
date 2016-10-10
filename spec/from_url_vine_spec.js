b2e871f6a52e839bac48fc77dd1b28eadeeb7f1d
Javier Rossetti <javier_rossetti1972@hotmail.com>
2016-10-10 17:53:00 +0100

    Minified gulp file

---
+++ spec/from_url_vine_spec.js
@@ -0,0 +1,29 @@
+/* jshint undef: false, unused: true */
+
+describe('CategorisedUrl.fromUrl', function(){
+
+  describe('Vine', function(){
+
+    describe('video', function(){
+      var url = 'https://vine.co/v/5gAphDzxlYt';
+      var catUrl = CategorisedUrl.fromUrl(url);
+
+      it('should return the provider', function(){
+        expect(catUrl.provider).toBe('vine');
+      });
+
+      it('should return the resource_type', function(){
+        expect(catUrl.resource_type).toBe('media');
+      });
+
+      it('should return the resource', function(){
+        expect(catUrl.resource).toBe('5gAphDzxlYt');
+      });
+
+      it('should return the canonical url', function(){
+        expect(catUrl.canonical_url).toBe('https://vine.co/v/5gAphDzxlYt');
+      });
+    });
+  });
+
+});
