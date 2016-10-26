59f8668b6e697d4a845fbd8513b823e018bf76dc
Fernando Bottin <fernando.bottin@storyful.com>
2016-10-26 14:52:23 +0100

    Get the proprer URL when comes from a redirect.

--- src/categorised-url.js
+++ src/categorised-url.js
@@ -19,98 +19,98 @@
     var providers = [
       {
         // YouTube User
-        pattern: /(?:https?:)?\/\/(?:www\.)?youtube\.com\/(?:channel\/|user\/|attribution_link\/)([a-zA-Z0-9]{1,})/,
+        pattern: /((?:https?:)?\/\/(?:www\.)?youtube\.com\/(?:channel\/|user\/|attribution_link\/)([a-zA-Z0-9]{1,}))/,
         provider: 'youtube',
         resource_type: 'user',
         canonical_url: 'https://www.youtube.com/user/{RESOURCE}/featured'
       },
       {
         // YouTube Media
-        pattern: /(?:https?:)?\/{2}(?:(?:www|m)\.)?(?:(?:youtu\.be)|(?:youtube\.com)){1}\/?(?:attribution_link)?(?:(?:v\/)|(?:\/u\/\w\/)|(?:embed\/)|(?:watch\?))?\??(?:(?:t=\S*&|u=\S*&)+)?(?:v=)?(?:v=)?(?!channel|user|results)([A-Za-z0-9-_]+).*/,
+        pattern: /((?:https?:)?\/{2}(?:(?:www|m)\.)?(?:(?:youtu\.be)|(?:youtube\.com)){1}\/?(?:attribution_link)?(?:(?:v\/)|(?:\/u\/\w\/)|(?:embed\/)|(?:watch\?))?\??(?:(?:t=\S*&|u=\S*&)+)?(?:v=)?(?:v=)?(?!channel|user|results)([A-Za-z0-9-_]+).*)/,
         provider: 'youtube',
         resource_type: 'media',
         canonical_url: 'https://www.youtube.com/watch?v={RESOURCE}'
       },
       {
         // Instagram User
-        pattern: /(?:https?:)?\/\/(?:www\.)?(?:instagr\.am|instagram\.com)\/(?!p\/)([\w\.]*)(?:\/)?$/,
+        pattern: /((?:https?:)?\/\/(?:www\.)?(?:instagr\.am|instagram\.com)\/(?!p\/)([\w\.]*)(?:\/)?$)/,
         provider: 'instagram',
         resource_type: 'user',
         canonical_url: 'https://www.instagram.com/p/{RESOURCE}/'
       },
       {
         // Instagram Media
-        pattern: /(?:https?:)?\/\/(?:www\.)?(?:insta)(?:gr\.am|gram\.com)\/p\/([a-zA-Z0-9-_]+)(?:\/|\?){0,2}.*$/,
+        pattern: /((?:https?:)?\/\/(?:www\.)?(?:insta)(?:gr\.am|gram\.com)\/p\/([a-zA-Z0-9-_]+)(?:\/|\?){0,2}.*$)/,
         provider: 'instagram',
         resource_type: 'media',
         canonical_url: 'https://www.instagram.com/p/{RESOURCE}/'
       },
       {
         // Facebook Media: videos
-        pattern: /(?:https?:)?\/\/(?:www\.)facebook\.com\/(?:video\/)?(?:video\.php\?v=(\d+)|\S+\/videos\/(?:vb\.\S+\/)?(\d+))\/?.*$/,
+        pattern: /((?:https?:)?\/\/(?:www\.)facebook\.com\/(?:video\/)?(?:video\.php\?v=(\d+)|\S+\/videos\/(?:vb\.\S+\/)?(\d+))\/?.*$)/,
         provider: 'facebook',
         resource_type: 'media',
-        canonical_url: '{URL}'
+        canonical_url: 'https://www.facebook.com/video.php?v={RESOURCE}'
       },
       {
         // Facebook Media: photo
-        pattern: /(?:https?:)?\/\/(?:www\.)facebook\.com\/(?:(?:[a-zA-Z0-9\.]+\/?photos\/(?:[a-zA-Z0-9\.-]+\/)?[a-zA-Z0-9\.-]+\/(\d+))|(?:photo.php\?fbid=(\d+)\&?))+(.+)?/,
+        pattern: /((?:https?:)?\/\/(?:www\.)facebook\.com\/(?:(?:[a-zA-Z0-9\.]+\/?photos\/(?:[a-zA-Z0-9\.-]+\/)?[a-zA-Z0-9\.-]+\/(\d+))|(?:photo.php\?fbid=(\d+)\&?))+(.+)?)/,
         provider: 'facebook',
         resource_type: 'media',
         canonical_url: 'https://www.facebook.com/photo.php?fbid={RESOURCE}'
       },
       {
         // Facebook Media: posts
-        pattern: /(?:https?:)?\/\/(?:www\.)facebook\.com\/(?:[a-zA-Z0-9\.]+\/?)(?:posts\/)+(\d+)/,
+        pattern: /((?:https?:)?\/\/(?:www\.)facebook\.com\/(?:[a-zA-Z0-9\.]+\/?)(?:posts\/)+(\d+))/,
         provider: 'facebook',
         resource_type: 'media',
         canonical_url: '{URL}'
       },
       {
         // Facebook Media: stories
-        pattern: /(?:https?:)?\/\/(?:www\.)facebook\.com\/permalink\.php\?(.+)/,
+        pattern: /((?:https?:)?\/\/(?:www\.)facebook\.com\/permalink\.php\?(.+))/,
         provider: 'facebook',
         resource_type: 'media',
         canonical_url: '{URL}'
       },
       {
         // Facebook Profile
-        pattern: /(?:https?:)?\/\/(?:www\.)facebook\.com\/(?:[a-zA-Z0-9\.]+\/?)/,
+        pattern: /((?:https?:)?\/\/(?:www\.)facebook\.com\/(?:[a-zA-Z0-9\.]+\/?))/,
         provider: 'facebook',
         resource_type: 'user',
         canonical_url: 'https://www.facebook.com/{RESOURCE}'
       },
       {
         // Twitter Media
-        pattern: /(?:https?:)?\/\/(www\.)?twitter\.com\/[_a-zA-Z0-9]{0,}.\/status\/([0-9]{1,})\??(?:\S+)?$/,
+        pattern: /((?:https?:)?\/\/(www\.)?twitter\.com\/[_a-zA-Z0-9]{0,}.\/status\/([0-9]{1,})\??(?:\S+)?$)/,
         provider: 'twitter',
         resource_type: 'media',
         canonical_url: '{URL}'
       },
       {
         // Twitter Profile
-        pattern: /(?:https?:)?\/\/(?:www\.)?twitter\.com\/([_a-zA-Z0-9]{0,}.)\/?$/,
+        pattern: /((?:https?:)?\/\/(?:www\.)?twitter\.com\/([_a-zA-Z0-9]{0,}.)\/?$)/,
         provider: 'twitter',
         resource_type: 'user',
         canonical_url: 'categorisedUrl'
       },
       {
         // Vimeo Media
-        pattern: /.*vimeo\..*\/(\d+)/,
+        pattern: /(.*vimeo\..*\/(\d+))/,
         provider: 'vimeo',
         resource_type: 'media',
         canonical_url: 'https://vimeo.com/{RESOURCE}'
       },
       {
         // Vine Resource
-        pattern: /.*vine\.co\/v\/([a-zA-Z0-9]+)/,
+        pattern: /(.*vine\.co\/v\/([a-zA-Z0-9]+))/,
         provider: 'vine',
         resource_type: 'media',
         canonical_url: 'https://vine.co/v/{RESOURCE}' //https://vine.co/v/5gAphDzxlYt
       },
       {
         // Vine Profile
-        pattern: /.*vine\.co\/([a-zA-Z0-9]+)/,
+        pattern: /(.*vine\.co\/([a-zA-Z0-9]+))/,
         provider: 'vine',
         resource_type: 'user',
         canonical_url: 'https://vine.co/{RESOURCE}' //https://vine.co/mmitchelldaviss?mode=list
@@ -122,6 +122,10 @@
     };
 
     var parseResource = function(pattern, url){
+      return pattern.exec(url).filter(notEmpty)[2];
+    };
+
+    var parseUrl = function(pattern, url){
       return pattern.exec(url).filter(notEmpty)[1];
     };
 
@@ -145,7 +149,7 @@
         provider = providers[i];
 
         if( decodedUrl.match(provider.pattern) ){
-          categorisedUrl.url            = decodedUrl;
+          categorisedUrl.url            = parseUrl(provider.pattern, decodedUrl);
           categorisedUrl.provider       = provider.provider;
           categorisedUrl.resource_type  = provider.resource_type;
           categorisedUrl.resource       = parseResource(provider.pattern, decodedUrl);
